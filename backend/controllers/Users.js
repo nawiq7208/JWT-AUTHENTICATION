83039fd7ecf330fd7b656cf972403d9dffefa106
nawiq7208 <ipsurbakti7208@gmail.com>
2022-06-20 11:24:52 +0800

    initial commit

---
+++ backend/controllers/Users.js
@@ -0,0 +1,32 @@
+import { response } from "express";
+import Users from "../models/UserModel.js";
+import bcrypt from "bcrypt";
+
+export const getUsers = async (req, res) => {
+	try {
+		const users = await Users.findAll();
+		res.json(users);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export const Register = async (req, res) => {
+	const { name, email, password, confPassword } = req.body;
+	if (password !== confPassword)
+		return res
+			.status(400)
+			.json({ msg: "Password and Confirm Password are not match" });
+	const salt = await bcrypt.genSalt();
+	const hashPassword = await bcrypt.hash(password, salt);
+	try {
+		await Users.create({
+			name: name,
+			email: email,
+			password: hashPassword,
+		});
+		res.json({ mesg: "Register Successfuly" });
+	} catch (error) {
+		console.log(error);
+	}
+};
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,17 @@
 import express from "express";
+import db from "./config/Database.js";
+import router from "./routes/index.js";
 
 const app = express();
+
+try {
+	await db.authenticate();
+	console.log("Database Connected...");
+} catch (error) {
+	console.error(error);
+}
+
+app.use(express.json());
+app.use(router);
 
 app.listen(5000, () => console.log("Server running at port 5000"));
